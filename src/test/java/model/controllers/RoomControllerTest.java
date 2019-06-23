90387ba5cc5bbcdef088527377a4487174ce9762
RobekX <robek005@gmail.com>
2019-06-24 01:12:22 +0200

    Test Client

--- src/test/java/model/controllers/RoomControllerTest.java
+++ src/test/java/model/controllers/RoomControllerTest.java
@@ -3,13 +3,16 @@ package model.controllers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
-
+@RunWith(MockitoJUnitRunner.class)
 public class RoomControllerTest {
 
     @Before
     public void setUp() throws Exception {
+
     }
 
     @After
